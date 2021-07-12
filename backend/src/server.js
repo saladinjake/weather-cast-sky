da1f980d44fcf3bd100fa24cc58869b6424bf790
saladinjake <juwavicto@gmail.com>
2021-07-12 15:00:00 +0100

    fix: resolve docker backend port exposure and frontend build errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ app.get('/api/health', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 }
 
 module.exports = app;
